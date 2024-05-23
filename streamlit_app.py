921e0878c4ad72e146b889002b7a45ca3c68350f
Ignat Vedernikov <smicurin474@gmail.com>
2024-05-23 10:13:11 +0500

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from sklearn.metrics import jaccard_score
 from sklearn.model_selection import train_test_split
 from nltk.stem import PorterStemmer
@@ -40,14 +41,6 @@ def load_model():
     rf = RandomForestClassifier(n_estimators=150, max_depth=None, n_jobs=-1)
     model = rf.fit(X_train_vect, y_train)
     return model
-
-
-def clean_text(text):
-    text = "".join([word.lower()
-                   for word in text if word not in string.punctuation])
-    tokens = re.split('\W+', text)
-    text = [ps.stem(word) for word in tokens if word not in stopwords]
-    return text
 
 
 tfidf_vect = TfidfVectorizer(analyzer=clean_text, max_features=500)
